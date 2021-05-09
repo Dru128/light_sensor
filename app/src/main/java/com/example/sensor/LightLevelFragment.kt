76ad1b9b1966ae18dfc4d86cd80d9c795fe54763
Andrey Shaidurov <andreu012a@mail.ru>
2021-05-10 00:33:26 +0700

    release

--- app/src/main/java/com/example/sensor/LightLevelFragment.kt
+++ app/src/main/java/com/example/sensor/LightLevelFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 
@@ -23,11 +24,13 @@ class LightLevelFragment : Fragment() // Шайдуров Андрей
     private lateinit var max_edit_text: EditText
     private lateinit var min_edit_text: EditText
     private lateinit var imageView: ImageView
+    private lateinit var lighting_square: View
+    private lateinit var blackout_square: View
 
     private var maxLight: Int = 10000
     private var minLight: Int = 3000
-
     private var currentLight: Int? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         val root = inflater.inflate(R.layout.light_level_fragment, container, false)
@@ -37,6 +40,8 @@ class LightLevelFragment : Fragment() // Шайдуров Андрей
             min_edit_text = findViewById<EditText>(R.id.min_light_edit_text)
             light_text = findViewById<TextView>(R.id.progress_light_text)
             imageView = findViewById<ImageView>(R.id.imageView)
+            lighting_square = findViewById<View>(R.id.lighting)
+            blackout_square = findViewById<View>(R.id.blackout)
             findViewById<Button>(R.id.save_cur_light_max_button).setOnClickListener {
                 maxLight = currentLight!!
                 max_edit_text.setText(maxLight.toString())
@@ -56,13 +61,13 @@ class LightLevelFragment : Fragment() // Шайдуров Андрей
         }
         else
         {
-            max_edit_text.doAfterTextChanged { maxLight = it!!.toString().toInt() }
-            min_edit_text.doAfterTextChanged { minLight = it!!.toString().toInt() }
+            max_edit_text.doAfterTextChanged { if (it != null && it.isNotEmpty()) maxLight = it.toString().toInt() }
+            min_edit_text.doAfterTextChanged { if (it != null && it.isNotEmpty()) minLight = it.toString().toInt() }
         }
         return root
     }
 
-    var listenerLight: SensorEventListener = object : SensorEventListener
+    private var listenerLight: SensorEventListener = object : SensorEventListener
     {
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
         override fun onSensorChanged(event: SensorEvent)
@@ -92,14 +97,20 @@ class LightLevelFragment : Fragment() // Шайдуров Андрей
             light > maxLight -> // нужно затемнить
             {
                 imageView.setImageResource(R.drawable.umbrella)
+                blackout_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+                lighting_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
             }
             light < minLight -> // нужно включить подсветку
             {
                 imageView.setImageResource(R.drawable.lamp)
+                blackout_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
+                lighting_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
             }
             light in minLight..maxLight -> // нормальный уровень яркости
             {
                 imageView.setImageBitmap(null)
+                blackout_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
+                lighting_square.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
             }
         }
     }
